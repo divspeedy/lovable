ec0b805005271055b34e8b5d9bf34b4230ecdafb
Divesh Kumar <divesh.kumar01@indiamart.com>
2025-07-20 17:34:01 +0530

    change the host name

--- backend/server.py
+++ backend/server.py
@@ -71,4 +71,5 @@ def process_files_endpoint():
 
 # --- Main Entry Point ---
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # NEW, CORRECT LINE
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
